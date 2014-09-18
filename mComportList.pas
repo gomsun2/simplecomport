242a9051007b332eb6de51f2c3e034aa7446dea5
gomsun2 <gomsun2@gmail.com>
2014-09-18 12:59:24 +0900

    Create mComportList.pas

---
+++ mComportList.pas
@@ -0,0 +1,45 @@
+unit mComportList;
+
+interface
+
+uses
+  Classes, SysUtils, Windows, System.Generics.Collections;
+
+type
+  TComPortList = class(TStringList)
+  private
+  public
+    procedure Search;
+  end;
+
+implementation
+
+{ TComPortList }
+
+procedure TComPortList.Search;
+const
+  RegPath = 'HARDWARE\DEVICEMAP\SerialComm';
+var
+  Reg: TRegistry;
+  SL: TStringList;
+  i: Integer;
+begin
+  Reg := TRegistry.Create(KEY_READ);
+  SL := TStringList.Create;
+  try
+    Reg.RootKey := HKEY_LOCAL_MACHINE;
+    if not Reg.KeyExists(RegPath) then Exit;
+    if not Reg.OpenKey(RegPath, False) then
+      raise Exception.Create('TSerialComDeviceList.find_SerialComDevice: RegKey OpenFailed ');
+
+    Reg.GetValueNames(SL);
+    for i := 0 to SL.Count - 1 do
+      Values[Reg.ReadString(SL[i])] := SL[i];
+    Reg.CloseKey;
+  finally
+    FreeAndNil(SL);
+    FreeAndNil(Reg);
+  end;
+end;
+
+end.
